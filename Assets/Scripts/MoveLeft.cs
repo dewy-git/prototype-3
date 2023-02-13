471a3c7acd0583ec01f575fd0197e88e5884ad19
dewy-git <dewu@s.sfusd.edu>
2023-02-13 14:07:49 -0800

    completed step 6.5 of unit 3.2 (moveleft.cs)

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -6,18 +6,25 @@ public class MoveLeft : MonoBehaviour
 {
     public float speed = 30;
     private PlayerController playerControllerScript;
+    private float leftBound = -15;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerControllerScript = 
-        GameObject.Find("Player").GetComponent<PlayerController>();
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (playerControllerScript.gameOver == false)
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if (transform.position.x <leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
